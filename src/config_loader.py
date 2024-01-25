84ea64b60263b4d32036a5471c8dc1368fa54903
Maria Barrientos Moreno <marisol.barrientos@tum.de>
2024-01-25 22:26:19 +0100

    Change absolute root to relative :)

--- src/config_loader.py
+++ src/config_loader.py
@@ -1,12 +1,15 @@
 import json
-
+import os
 
 class ConfigLoader:
     def __init__(self, config_path):
-        self.config_path = config_path
+        # Get the absolute path of the directory where the script is located
+        self.script_dir = os.path.dirname(__file__)
+        self.parent_dir = os.path.dirname(self.script_dir)
+        # Construct the absolute path of the config file
+        self.config_path = os.path.join(self.parent_dir, config_path)
         self.config_data = self.load_config()
 
     def load_config(self):
         with open(self.config_path, 'r') as config_file:
             return json.load(config_file)
-
--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+import os
+
 from json_to_html import JsonToHtmlConverter
 from config_loader import ConfigLoader
 from file_handler import FileHandler
@@ -7,27 +9,30 @@ import json
 
 
 def main():
-    config_loader = ConfigLoader('config.json')
+    config_loader = ConfigLoader("./config.json")
     config_data = config_loader.config_data
+    script_dir = os.path.dirname(__file__)
+    parent_dir = os.path.dirname(script_dir)
 
     file_handler = FileHandler()
     api_handler = APIHandler(config_data['api_key'], config_data['model_name'])
 
+
     try:
-        input_text = file_handler.read_file(config_data['input_file'])
-        prompt = file_handler.read_file(config_data['prompt_file']).replace(
+        input_text = file_handler.read_file(parent_dir + config_data['input_file'])
+        prompt = file_handler.read_file(parent_dir + config_data['prompt_file']).replace(
             "NATURAL_LANGUAGE_TEXT_HERE", input_text
         )
 
         api_response = api_handler.call_gpt4_chat_api(prompt)
-        file_handler.write_file(api_response, config_data['in_between_output_file'])
+        file_handler.write_file(api_response, parent_dir + config_data['in_between_output_file'])
 
         processed_data = json.loads(api_response['choices'][0]['message']['content'])
-        file_handler.write_file(processed_data, config_data['output_file'])
+        file_handler.write_file(processed_data, parent_dir + config_data['output_file'])
 
-        print("Output saved to", config_data['output_file'])
+        print("Output saved to", parent_dir + config_data['output_file'])
 
-        converter = JsonToHtmlConverter(config_data['output_file'], config_data['html_output_file'])
+        converter = JsonToHtmlConverter(parent_dir + config_data['output_file'], parent_dir + config_data['html_output_file'])
         converter.convert()
 
     except Exception as e:
